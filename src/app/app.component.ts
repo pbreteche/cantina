e95667f0a7eda5d770570bb80f6525a93cc0de89
Pierre Bretéché <pbreteche@dawan.fr>
2017-06-13 12:16:38 +0200

    Intégration de la classe métier "Restaurant" fix #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Restaurant} from './restaurant';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  resto: Restaurant;
+
+  constructor() {
+    this.resto = new Restaurant();
+    this.resto.name = 'La P\'tite Ardoise';
+    this.resto.address = 'Bd Vincent Gache';
+  }
 }
